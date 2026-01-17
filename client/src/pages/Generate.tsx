1ec09e30ea5449a8c1cc7854aa8ba671ab5bb935
siddharthNirmale <siddharth175nirmale1@gmail.com>
2026-01-17 20:08:54 +0530

    Enhance Generate component with state management and SoftBackdrop integration

--- client/src/pages/Generate.tsx
+++ client/src/pages/Generate.tsx
@@ -1,16 +1,27 @@
-import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import type { IThumbnail } from '../assets/assets';
+import SoftBackdrop from '../components/SoftBackdrop';
 
 const Generate = () => {
   
   const {id} = useParams();
   const [title,setTitle] = useState('');
   const [additonalDetails,setAdditonalDetails] = useState('');
+  const [thumbnail,setThumbnail] = useState<IThumbnail | null>(null);
+  const [loading,setLoading] =useState(false);
+
+  
   
   return (
+    <>
+    <SoftBackdrop/>
     <div>
       
     </div>
+
+      
+    </>
   )
 }
 
