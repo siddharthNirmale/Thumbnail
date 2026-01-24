2d8a6b204f430c77736163a6f14cf07d9e7a143d
siddharthNirmale <siddharth175nirmale1@gmail.com>
2026-01-24 23:37:47 +0530

    setup db

---
+++ server/configs/db.ts
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const connectDb = async ()=>{
+    try{
+mongoose.connection.on('connected',()=>console.log("MongoDB connected "))
+await mongoose.connect(process.env.MONGODB_URL as string);
+    }catch(error){
+        console.error('Error connecting to MongoDB: ',error);
+    }
+}
+
+export default connectDb;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,17 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDb from '../configs/db.js';
+
+await connectDb();
 
 const app = express();
 
-const port = 3000;
+const port = process.env.port || 3000 ;
+
+app.use(cors());
+
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
