931ee0d8c1f011dff6c8e99d370156e006ef1ae3
siddharthNirmale <siddharth175nirmale1@gmail.com>
2026-02-24 19:56:13 +0530

    did some changes in links

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -39,16 +39,16 @@ export default function Footer() {
             >
                 <p className="max-w-60">Making every customer feel valued—no matter the size of your audience.</p>
                 <div className="flex items-center gap-4 mt-3">
-                    <a href="https://dribbble.com/prebuiltui" target="_blank" rel="noreferrer">
+                    <a href="#" target="_blank" rel="noreferrer">
                         <DribbbleIcon className="size-5 hover:text-indigo-500" />
                     </a>
-                    <a href="https://www.linkedin.com/company/prebuiltui" target="_blank" rel="noreferrer">
+                    <a href="https://www.linkedin.com/company/siddharth-nirmale" target="_blank" rel="noreferrer">
                         <LinkedinIcon className="size-5 hover:text-indigo-500" />
                     </a>
-                    <a href="https://x.com/prebuiltui" target="_blank" rel="noreferrer">
+                    <a href="https://x.com/xosmos" target="_blank" rel="noreferrer">
                         <TwitterIcon className="size-5 hover:text-indigo-500" />
                     </a>
-                    <a href="https://www.youtube.com/@prebuiltui" target="_blank" rel="noreferrer">
+                    <a href="#" target="_blank" rel="noreferrer">
                         <YoutubeIcon className="size-6 hover:text-indigo-500" />
                     </a>
                 </div>
